c961638fe11903e2e31033c3ca224e14a20edaef
Zuhra66 <157528617+Zuhra66@users.noreply.github.com>
2024-04-16 09:55:05 -0700

    sign up page created

---
+++ app/src/main/java/com/example/eco/database/SignUpActivity.java
@@ -0,0 +1,20 @@
+package com.example.eco.database;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.example.eco.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_sign_up);
+
+        });
+    }
+}
